658681c3f56414c4b3a8154b929d7eb8b70cf6af
lswmxl <128008687+lswmxl@users.noreply.github.com>
2024-11-24 22:46:32 +0800

    Update hysteria.sh

--- hysteria2-install-main/hy2/hysteria.sh
+++ hysteria2-install-main/hy2/hysteria.sh
@@ -222,7 +222,7 @@ insthysteria(){
     fi
     ${PACKAGE_INSTALL} curl wget sudo qrencode procps iptables-persistent netfilter-persistent
 
-    wget -N https://raw.githubusercontent.com/Misaka-blog/hysteria-install/main/hy2/install_server.sh
+    wget -N https://5th.pp.ua/https://raw.githubusercontent.com/lswmxl/hy2-in/refs/heads/main/hysteria2-install-main/hy2/install_server.sh
     bash install_server.sh
     rm -f install_server.sh
 
